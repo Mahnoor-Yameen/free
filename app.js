963dc70dbe7818dccb5d947684be7fe2d8a5703f
Mahnoor-Yameen <57858630+Mahnoor-Yameen@users.noreply.github.com>
2023-04-06 15:46:48 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,122 @@
+//Day - 1     6-April-2023
+
+//Chapter: 01   Alerts
+
+alert("Thanks for your input!");
+
+alert ( "Thanks for your input!" );
+
+//Chapter: 02  Variables for Strings
+
+var name = "Mark";
+var name = "Mark";
+name = "Ace";
+
+
+var nationality;
+nationality = "U.S.";
+
+var floogle = "Mark";
+var name = "Mark";
+var floogle = "Mark";
+
+var lessonAuthor = "Mark";
+var guyWhoKeepsSayingHisOwnName = "Mark";
+var x = "Mark";
+
+var lastName = "Smith";
+var cityOfOrigin = "New Orleans";
+var aussieGreeting = "g'Day";
+
+
+var nickname = "Bub";
+
+alert("Thanks for your input!");
+
+var thanx = "Thanks for your input!"
+alert(thanx);
+
+
+// Chapter: 03    Variables for numbers
+
+var weight = 150;
+
+weight + 25
+
+var originalNum = 23;
+var newNum = originalNum + 7;
+
+
+var originalNum = 23;
+var numToBeAdded = 7;
+var newNum = originalNum + numToBeAdded;
+
+var originalNum = 90;
+originalNum = originalNum + 10;
+
+var originalNum = "23";
+var newNum = originalNum + 7;
+
+1 var originalNumber = 23;
+2 var newNumber = originalNumber + 7;
+
+
+var 1stPresident = "Washington";
+
+var prezWhoCame1st = "Washington";
+
+alert(144);
+
+var caseQty = 144;
+alert(caseQty);
+
+
+//Chapter: 04   Variable Names Legal and Illegal
+
+userResponse
+userResponseTime
+userResponseTimeLimit
+response
+
+//Chapter: 05  Math expressions: Familiar operators
+
+var popularNumber = 4;
+
+var popularNumber = 2 + 2;
+
+alert(2 + 2);
+
+var popularNumber = 12 - 24;
+
+var popularNumber = 3 * 12;
+
+1 var num = 10;
+2 var popularNumber = num + 200;
+
+1 var num = 10;
+2 var anotherNum = 1;
+3 var popularNumber = num + anotherNum;
+
+var whatsLeftOver = 10 % 3;
+
+var whatsLeftOver = 9 % 3;
+
+//Chapter: 06  Math expressions: Unfamiliar operators
+
+num++;
+num = num + 1;
+
+num--;
+
+1 var num = 1;
+2 var newNum = num++;
+
+1 var num = 1;
+2 var newNum = ++num;
+
+1 var num = 1;
+2 var newNum = num--;
+
+1 var num = 1;
+2 var newNum = --num;
+
